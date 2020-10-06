a8ce08d372a31bcfd1cbcaa4209490aed504049b
BasharKing <54625068+BasharKing@users.noreply.github.com>
2020-10-06 12:19:00 +0300

    Update coins.js

--- commands/coins/coins.js
+++ commands/coins/coins.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/money", {
-    useNewUrlParser: true
+const mongoPath = "mongodb+srv://multig:L3j26ySPbqQgfjKd@cluster0.bhj3c.mongodb.net/multigdb?retryWrites=true&w=majority"
+mongoose.connect(mongoPath, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 module.exports = {
