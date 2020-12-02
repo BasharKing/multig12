25db706cf48e185747c100809495eb198f822830
BasharKing <basharking186@gmail.com>
2020-12-02 18:58:44 +0200

    init3

--- commands/coins/shop.js
+++ commands/coins/shop.js
@@ -13,14 +13,14 @@ module.exports = {
     execute(client, message, args) {
 
         const embed = new Discord.MessageEmbed()
-        .setAuthor("Items Shop | MG Shop")
+        .setAuthor("Items Shop | MG Shop", message.guild.iconURL({ dynamic: true }))
         .setColor("#36393f")
-        .addField("Private Room", "**Cost:** 100 Coins", true)
-        .addField("Private Room w/Special Role", "**Cost:** 350 Coins", true)
-        .addField("Special Role", "**Cost:** 250 Coins", true)
-        .addField("Create Gang", "**Cost:** 300 Coins", true)
-        .addField("Music Bot | Soon", "**Cost:** 200 Coins", true)
-        .addField("Note", "All items is for 1month/ You can renew it by talking to .bashaàrI ⚘#7069")
+        .addField("Private Room:", "**Cost:** 100 Coins", true)
+        .addField("Private Room w/Special Role:", "**Cost:** 350 Coins", true)
+        .addField("Special Role:", "**Cost:** 250 Coins", true)
+        .addField("Create Gang:", "**Cost:** 300 Coins", true)
+        .addField("Music Bot | Soon:", "**Cost:** Not specified", true)
+        .addField("Note:", "All items is for 1month/ You can renew it by talking to **.bashaàrI ⚘#7069**")
         .setTimestamp()
         .setFooter(message.author.username, message.author.displayAvatarURL({ dynamic: true }));
 
